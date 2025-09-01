89050b6c29decd37c0bf73880494e4bf2f639788
Eduardo Fontenele <eduardofonteneledev@gmail.com>
2025-09-01 16:01:27 -0300

    Initial commit

---
+++ src/main/java/br/com/sunnypay/SunnypayApplication.java
@@ -0,0 +1,13 @@
+package br.com.sunnypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnypayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunnypayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sunnypay/SunnypayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sunnypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunnypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
